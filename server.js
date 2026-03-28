a54a49db139eff2e4dbbb207e68bc273addc29eb
dawaisthename <seifudawde@gmail.com>
2026-03-28 11:46:47 +0300

    fix:added an upload middleware for the route

--- server.js
+++ server.js
@@ -21,13 +21,14 @@ mongoose
   .then(() => console.log("✅ MongoDB Connected Successfully"))
   .catch((err) => console.error("❌ MongoDB Connection Error:", err));
 
+// --- ADD THIS LINE ---
+app.use("/uploads", express.static("uploads"));
 // Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/news", newsRoutes);
 app.use("/api/careers", jobRoutes); // Added this line to include job routes
 app.use("/api/contacts", contactRoutes);
 app.use("/api/applications", ApplicationsRoutes); // Added this line to include application routes
-
 // Base Route for testing
 app.get("/", (req, res) => {
   res.send("Kaffa API is live...");
